76cb95f1c64ccbc8e88a64b638f4bfc6550ebd71
System User <yann@heretix6.qnocart.com>
2014-12-02 10:41:53 +0100

    GCC 4.8.3

---
+++ gcc-4.8.3-64/lib/gcc/x86_64-unknown-linux-gnu/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/yann/64_build/gcc-4.8.3 --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
